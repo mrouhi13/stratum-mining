92162980e75dfcddf8cfd68a08b7ca13d4c2251d
Majid Rouhi <mrouhi13@gmail.com>
2020-04-16 21:16:37 +0430

    Added SegWit Support

--- lib/bitcoin_rpc.py
+++ lib/bitcoin_rpc.py
@@ -1,20 +1,25 @@
-'''
+"""
     Implements simple interface to a coin daemon's RPC.
-'''
+"""
+
+import base64
 
 import simplejson as json
-import base64
 from twisted.internet import defer
 from twisted.web import client
-import time
 
 import lib.logger
+import lib.settings as settings
+import util
+
 log = lib.logger.get_logger('bitcoin_rpc')
 
+gbt_known_rules = ["segwit"]
+
+
 class BitcoinRPC(object):
-    
+
     def __init__(self, host, port, username, password):
-        log.debug("Got to Bitcoin RPC")
         self.bitcoin_url = 'http://%s:%d' % (host, port)
         self.credentials = base64.b64encode("%s:%s" % (username, password))
         self.headers = {
@@ -22,7 +27,7 @@ class BitcoinRPC(object):
             'Authorization': 'Basic %s' % self.credentials,
         }
         client.HTTPClientFactory.noisy = False
-	self.has_submitblock = False        
+        self.has_submitblock = False
 
     def _call_raw(self, data):
         client.Headers
@@ -32,125 +37,113 @@ class BitcoinRPC(object):
             headers=self.headers,
             postdata=data,
         )
-           
+
     def _call(self, method, params):
         return self._call_raw(json.dumps({
-                'jsonrpc': '2.0',
-                'method': method,
-                'params': params,
-                'id': '1',
-            }))
-    
+            'jsonrpc': '2.0',
+            'method': method,
+            'params': params,
+            'id': '1',
+        }))
+
     @defer.inlineCallbacks
     def check_submitblock(self):
         try:
-            log.info("Checking for submitblock")
             resp = (yield self._call('submitblock', []))
-	    self.has_submitblock = True
+            self.has_submitblock = True
         except Exception as e:
-            if (str(e) == "404 Not Found"):
-                log.debug("No submitblock detected.")
-		self.has_submitblock = False
-            elif (str(e) == "500 Internal Server Error"):
-                log.debug("submitblock detected.")
-		self.has_submitblock = True
+            if str(e) == "404 Not Found":
+                self.has_submitblock = False
+            elif str(e) == "500 Internal Server Error":
+                self.has_submitblock = True
             else:
-                log.debug("unknown submitblock check result.")
-		self.has_submitblock = True
+                self.has_submitblock = True
         finally:
-              defer.returnValue(self.has_submitblock)
+            defer.returnValue(self.has_submitblock)
 
-    
     @defer.inlineCallbacks
     def submitblock(self, block_hex, hash_hex, scrypt_hex):
-  #try 5 times? 500 Internal Server Error could mean random error or that TX messages setting is wrong
+        # try 5 times? 500 Internal Server Error could mean random error
+        # or that TX messages setting is wrong
         attempts = 0
         while True:
             attempts += 1
-            if self.has_submitblock == True:
+            if self.has_submitblock:
                 try:
-                    log.debug("Submitting Block with submitblock: attempt #"+str(attempts))
-                    log.debug([block_hex,])
-                    resp = (yield self._call('submitblock', [block_hex,]))
-                    log.debug("SUBMITBLOCK RESULT: %s", resp)
+                    log.debug([block_hex])
+                    resp = (yield self._call('submitblock', [block_hex, ]))
                     break
                 except Exception as e:
                     if attempts > 4:
-                        log.exception("submitblock failed. Problem Submitting block %s" % str(e))
+                        log.exception(
+                            "submitblock failed. Problem Submitting block %s" % str(
+                                e))
+                        log.exception("Try Enabling TX Messages in config.py!")
+                        raise
+            else:
+                try:
+                    log.debug(
+                        "Submitting Block with getblocktemplate submit: attempt #" + str(
+                            attempts))
+                    log.debug([block_hex, ])
+                    resp = (yield self._call('getblocktemplate', [
+                        {'mode': 'submit', 'data': block_hex}]))
+                    break
+                except Exception as e:
+                    if attempts > 4:
+                        log.exception(
+                            "getblocktemplate submit failed. Problem Submitting block %s" % str(
+                                e))
                         log.exception("Try Enabling TX Messages in config.py!")
                         raise
                     else:
                         continue
-            elif self.has_submitblock == False:
-                try:
-                    log.debug("Submitting Block with getblocktemplate submit: attempt #"+str(attempts))
-                    log.debug([block_hex,])
-                    resp = (yield self._call('getblocktemplate', [{'mode': 'submit', 'data': block_hex}]))
-                    break
-                except Exception as e:
-                    if attempts > 4:
-                        log.exception("getblocktemplate submit failed. Problem Submitting block %s" % str(e))
-                        log.exception("Try Enabling TX Messages in config.py!")
-                        raise
-                    else:
-                        continue
-            else:  # self.has_submitblock = None; unable to detect submitblock, try both
-                try:
-                    log.debug("Submitting Block with submitblock")
-                    log.debug([block_hex,])
-                    resp = (yield self._call('submitblock', [block_hex,]))
-                    break
-                except Exception as e:
-                    try:
-                        log.exception("submitblock Failed, does the coind have submitblock?")
-                        log.exception("Trying GetBlockTemplate")
-                        resp = (yield self._call('getblocktemplate', [{'mode': 'submit', 'data': block_hex}]))
-                        break
-                    except Exception as e:
-                        if attempts > 4:
-                            log.exception("submitblock failed. Problem Submitting block %s" % str(e))
-                            log.exception("Try Enabling TX Messages in config.py!")
-                            raise
-                        else:
-                            continue
 
-        if json.loads(resp)['result'] == None:
+        if json.loads(resp)['result'] is None:
             # make sure the block was created.
-            log.info("CHECKING FOR BLOCK AFTER SUBMITBLOCK")
             defer.returnValue((yield self.blockexists(hash_hex, scrypt_hex)))
         else:
             defer.returnValue(False)
 
     @defer.inlineCallbacks
     def getinfo(self):
-         resp = (yield self._call('getinfo', []))
-         defer.returnValue(json.loads(resp)['result'])
-    
+        resp = (yield self._call('getinfo', []))
+        defer.returnValue(json.loads(resp)['result'])
+
     @defer.inlineCallbacks
     def getblocktemplate(self):
         try:
-            resp = (yield self._call('getblocktemplate', [{}]))
+            params = [{}]
+            if settings.COINDAEMON_HAS_SEGWIT:
+                params = [{"rules": gbt_known_rules}]
+                resp = (yield self._call('getblocktemplate', params))
             defer.returnValue(json.loads(resp)['result'])
         # if internal server error try getblocktemplate without empty {} # ppcoin
         except Exception as e:
-            if (str(e) == "500 Internal Server Error"):
+            if str(e) == "500 Internal Server Error":
                 resp = (yield self._call('getblocktemplate', []))
                 defer.returnValue(json.loads(resp)['result'])
             else:
                 raise
-                                                  
+
     @defer.inlineCallbacks
     def prevhash(self):
-        resp = (yield self._call('getwork', []))
         try:
-            defer.returnValue(json.loads(resp)['result']['data'][8:72])
+            resp = (yield self._call('getbestblockhash', []))
+            defer.returnValue(json.loads(resp)['result'])
         except Exception as e:
-            log.exception("Cannot decode prevhash %s" % str(e))
-            raise
-        
+            if str(e) == "500 Internal Server Error":
+                resp = (yield self._call('getwork', []))
+                defer.returnValue(
+                    util.reverse_hash(
+                        json.loads(resp)['result']['data'][8:72]))
+            else:
+                log.exception("Cannot decode prevhash %s" % str(e))
+                raise
+
     @defer.inlineCallbacks
     def validateaddress(self, address):
-        resp = (yield self._call('validateaddress', [address,]))
+        resp = (yield self._call('validateaddress', [address, ]))
         defer.returnValue(json.loads(resp)['result'])
 
     @defer.inlineCallbacks
@@ -164,7 +157,7 @@ class BitcoinRPC(object):
         blockheight = None
         # try both hash_hex and scrypt_hex to find block
         try:
-            resp = (yield self._call('getblock', [hash_hex,]))
+            resp = (yield self._call('getblock', [hash_hex, ]))
             result = json.loads(resp)['result']
             if "hash" in result and result['hash'] == hash_hex:
                 log.debug("Block found: %s" % hash_hex)
@@ -179,7 +172,7 @@ class BitcoinRPC(object):
 
         except Exception as e:
             try:
-                resp = (yield self._call('getblock', [scrypt_hex,]))
+                resp = (yield self._call('getblock', [scrypt_hex, ]))
                 result = json.loads(resp)['result']
                 if "hash" in result and result['hash'] == scrypt_hex:
                     valid_hash = scrypt_hex
@@ -193,24 +186,31 @@ class BitcoinRPC(object):
                     defer.returnValue(False)
 
             except Exception as e:
-                log.info("Cannot find block for hash_hex %s or scrypt_hex %s" % hash_hex, scrypt_hex)
+                log.info(
+                    "Cannot find block for hash_hex %s or scrypt_hex %s" % hash_hex,
+                    scrypt_hex)
                 defer.returnValue(False)
 
-        #after we've found the block, check the block with that height in the blockchain to see if hashes match
+        # after we've found the block, check the block with that height
+        # in the blockchain to see if hashes match
         try:
-            log.debug("checking block hash against hash of block height: %s", blockheight)
-            resp = (yield self._call('getblockhash', [blockheight,]))
+            log.debug("checking block hash against hash of block height: %s",
+                      blockheight)
+            resp = (yield self._call('getblockhash', [blockheight, ]))
             hash = json.loads(resp)['result']
             log.debug("hash of block of height %s: %s", blockheight, hash)
             if hash == valid_hash:
-                log.debug("Block confirmed: hash of block matches hash of blockheight")
+                log.debug(
+                    "Block confirmed: hash of block matches hash of blockheight")
                 defer.returnValue(True)
             else:
-                log.debug("Block invisible: hash of block does not match hash of blockheight")
+                log.debug(
+                    "Block invisible: hash of block does not match hash of blockheight")
                 defer.returnValue(False)
 
         except Exception as e:
-            # cannot get blockhash from height; block was created, so return true
+            # cannot get blockhash from height; block was created,
+            # so return true
             defer.returnValue(True)
         else:
             log.info("Cannot find block for %s" % hash_hex)
--- lib/block_template.py
+++ lib/block_template.py
@@ -1,69 +1,105 @@
-import StringIO
 import binascii
 import struct
 
-import util
-import merkletree
 import halfnode
-from coinbasetx import CoinbaseTransactionPOW
-from coinbasetx import CoinbaseTransactionPOS
+import lib.logger
+import merkletree
+import util
 from coinbasetx import CoinbaseTransaction
-import lib.logger
-log = lib.logger.get_logger('block_template')
 
-import lib.logger
 log = lib.logger.get_logger('block_template')
-
 
 # Remove dependency to settings, coinbase extras should be
 # provided from coinbaser
 import settings
 
+witness_nonce = b'\0' * 0x20
+witness_magic = b'\xaa\x21\xa9\xed'
+
+
+class TxBlob(object):
+    def __init__(self):
+        self.data = ''
+
+    def serialize(self):
+        return self.data
+
+    def deserialize(self, data):
+        self.data = data
+
+
 class BlockTemplate(halfnode.CBlock):
-    '''Template is used for generating new jobs for clients.
+    """Template is used for generating new jobs for clients.
     Let's iterate extranonce1, extranonce2, ntime and nonce
-    to find out valid coin block!'''
-    
+    to find out valid coin block!"""
+
     coinbase_transaction_class = CoinbaseTransaction
-    
+
     def __init__(self, timestamper, coinbaser, job_id):
-        log.debug("Got To  Block_template.py")
-        log.debug("Got To Block_template.py")
         super(BlockTemplate, self).__init__()
-        
-        self.job_id = job_id 
+
+        self.job_id = job_id
         self.timestamper = timestamper
         self.coinbaser = coinbaser
-        
-        self.prevhash_bin = '' # reversed binary form of prevhash
+
+        self.prevhash_bin = ''  # reversed binary form of prevhash
         self.prevhash_hex = ''
         self.timedelta = 0
         self.curtime = 0
         self.target = 0
-        #self.coinbase_hex = None 
+        self.witness = 0
         self.merkletree = None
-                
+
         self.broadcast_args = []
-        
+
         # List of 4-tuples (extranonce1, extranonce2, ntime, nonce)
         # registers already submitted and checked shares
         # There may be registered also invalid shares inside!
-        self.submits = [] 
-                
+        self.submits = []
+
     def fill_from_rpc(self, data):
-        '''Convert getblocktemplate result into BlockTemplate instance'''
-        
-        #txhashes = [None] + [ binascii.unhexlify(t['hash']) for t in data['transactions'] ]
-        txhashes = [None] + [ util.ser_uint256(int(t['hash'], 16)) for t in data['transactions'] ]
-        mt = merkletree.MerkleTree(txhashes)
-        if settings.COINDAEMON_Reward == 'POW':
-            coinbase = CoinbaseTransactionPOW(self.timestamper, self.coinbaser, data['coinbasevalue'],
-                                              data['coinbaseaux']['flags'], data['height'],
-                                              settings.COINBASE_EXTRAS)
+        """Convert getblocktemplate result into BlockTemplate instance"""
+
+        commitment = None
+        nTime = data['curtime'] if data.has_key('curtime') else None
+
+        if settings.COINDAEMON_HAS_SEGWIT:
+            hashes = [None] + [util.ser_uint256(int(t['hash'], 16)) for t in
+                               data['transactions']]
+            try:
+                txids = [None] + [util.ser_uint256(int(t['txid'], 16)) for t in
+                                  data['transactions']]
+                mt = merkletree.MerkleTree(txids)
+            except KeyError:
+                mt = merkletree.MerkleTree(hashes)
+
+            wmt = merkletree.MerkleTree(hashes).withFirst(binascii.unhexlify(
+                '0000000000000000000000000000000000000000000000000000000000000000'))
+            self.witness = SHA256.new(
+                SHA256.new(wmt + witness_nonce).digest()).digest()
+            commitment = b'\x6a' + struct.pack(">b", len(self.witness) + len(
+                witness_magic)) + witness_magic + self.witness
+            try:
+                default_witness = data['default_witness_commitment']
+                commitment_check = binascii.unhexlify(default_witness)
+                if commitment != commitment_check:
+                    print(
+                        "calculated witness does not match supplied one! This block probably will not be accepted!")
+                    commitment = commitment_check
+            except KeyError:
+                pass
+            self.witness = commitment[6:]
         else:
-            coinbase = CoinbaseTransactionPOS(self.timestamper, self.coinbaser, data['coinbasevalue'],
-                                              data['coinbaseaux']['flags'], data['height'],
-                                              settings.COINBASE_EXTRAS, data['curtime'])
+            txhashes = [None] + [util.ser_uint256(int(t['hash'], 16)) for t in
+                                 data['transactions']]
+            mt = merkletree.MerkleTree(txhashes)
+
+        coinbase = CoinbaseTransaction(self.timestamper, self.coinbaser,
+                                       data['coinbasevalue'],
+                                       data['coinbaseaux']['flags'],
+                                       data['height'],
+                                       commitment,
+                                       settings.COINBASE_EXTRAS, nTime)
 
         self.height = data['height']
         self.nVersion = data['version']
@@ -73,71 +109,76 @@ class BlockTemplate(halfnode.CBlock):
         self.hashMerkleRoot = 0
         self.nTime = 0
         self.nNonce = 0
-        self.vtx = [ coinbase, ]
-        
+        self.vtx = [coinbase, ]
+
         for tx in data['transactions']:
-            t = halfnode.CTransaction()
-            t.deserialize(StringIO.StringIO(binascii.unhexlify(tx['data'])))
+            t = TxBlob()
+            t.deserialize(binascii.unhexlify(tx['data']))
             self.vtx.append(t)
-            
+
         self.curtime = data['curtime']
-        self.timedelta = self.curtime - int(self.timestamper.time()) 
+        self.timedelta = self.curtime - int(self.timestamper.time())
         self.merkletree = mt
         self.target = util.uint256_from_compact(self.nBits)
-        
+
         # Reversed prevhash
-        self.prevhash_bin = binascii.unhexlify(util.reverse_hash(data['previousblockhash']))
+        self.prevhash_bin = binascii.unhexlify(
+            util.reverse_hash(data['previousblockhash']))
         self.prevhash_hex = "%064x" % self.hashPrevBlock
-        
+
         self.broadcast_args = self.build_broadcast_args()
-                
+
     def register_submit(self, extranonce1, extranonce2, ntime, nonce):
-        '''Client submitted some solution. Let's register it to
-        prevent double submissions.'''
-        
+        """Client submitted some solution. Let's register it to
+        prevent double submissions."""
+
         t = (extranonce1, extranonce2, ntime, nonce)
         if t not in self.submits:
             self.submits.append(t)
             return True
         return False
-            
+
     def build_broadcast_args(self):
-        '''Build parameters of mining.notify call. All clients
+        """Build parameters of mining.notify call. All clients
         may receive the same params, because they include
         their unique extranonce1 into the coinbase, so every
-        coinbase_hash (and then merkle_root) will be unique as well.'''
+        coinbase_hash (and then merkle_root) will be unique as well."""
         job_id = self.job_id
         prevhash = binascii.hexlify(self.prevhash_bin)
-        (coinb1, coinb2) = [ binascii.hexlify(x) for x in self.vtx[0]._serialized ]
-        merkle_branch = [ binascii.hexlify(x) for x in self.merkletree._steps ]
+        (coinb1, coinb2) = [binascii.hexlify(x) for x in
+                            self.vtx[0]._serialized]
+        merkle_branch = [binascii.hexlify(x) for x in self.merkletree._steps]
         version = binascii.hexlify(struct.pack(">i", self.nVersion))
         nbits = binascii.hexlify(struct.pack(">I", self.nBits))
         ntime = binascii.hexlify(struct.pack(">I", self.curtime))
         clean_jobs = True
-        
-        return (job_id, prevhash, coinb1, coinb2, merkle_branch, version, nbits, ntime, clean_jobs)
+
+        return (
+            job_id, prevhash, coinb1, coinb2, merkle_branch, version, nbits,
+            ntime,
+            clean_jobs)
 
     def serialize_coinbase(self, extranonce1, extranonce2):
-        '''Serialize coinbase with given extranonce1 and extranonce2
-        in binary form'''
+        """Serialize coinbase with given extranonce1 and extranonce2
+        in binary form"""
         (part1, part2) = self.vtx[0]._serialized
         return part1 + extranonce1 + extranonce2 + part2
-    
+
     def check_ntime(self, ntime):
-        '''Check for ntime restrictions.'''
+        """Check for ntime restrictions."""
         if ntime < self.curtime:
             return False
-        
+
         if ntime > (self.timestamper.time() + 7200):
             # Be strict on ntime into the near future
             # may be unnecessary
             return False
-        
+
         return True
 
     def serialize_header(self, merkle_root_int, ntime_bin, nonce_bin):
-        '''Serialize header for calculating block hash'''
-        r  = struct.pack(">i", self.nVersion)
+        """Serialize header for calculating block hash"""
+        r = struct.pack(">i", self.nVersion)
         r += self.prevhash_bin
         r += util.ser_uint256_be(merkle_root_int)
         if settings.COINDAEMON_ALGO == 'riecoin':
@@ -146,15 +187,46 @@ class BlockTemplate(halfnode.CBlock):
         else:
             r += ntime_bin
             r += struct.pack(">I", self.nBits)
-        r += nonce_bin    
-        return r       
+        r += nonce_bin
+        return r
 
-    def finalize(self, merkle_root_int, extranonce1_bin, extranonce2_bin, ntime, nonce):
-        '''Take all parameters required to compile block candidate.
-        self.is_valid() should return True then...'''
-        
+    def finalize(self, merkle_root_int, extranonce1_bin, extranonce2_bin,
+                 ntime, nonce):
+        """Take all parameters required to compile block candidate.
+        self.is_valid() should return True then..."""
+
         self.hashMerkleRoot = merkle_root_int
         self.nTime = ntime
         self.nNonce = nonce
-        self.vtx[0].set_extranonce(extranonce1_bin + extranonce2_bin)        
-        self.sha256 = None # We changed block parameters, let's reset sha256 cache
+        self.vtx[0].set_extranonce(extranonce1_bin + extranonce2_bin)
+        self.sha256 = None  # We changed block parameters, let's reset sha256 cache
+
+    def serialize(self):
+        r = [struct.pack("<i", self.nVersion),
+             util.ser_uint256(self.hashPrevBlock),
+             util.ser_uint256(self.hashMerkleRoot),
+             struct.pack("<I", self.nTime), struct.pack("<I", self.nBits),
+             struct.pack("<I", self.nNonce), util.ser_vector(self.vtx)]
+        return ''.join(r)
+
+    def is_valid(self):
+        self.calc_sha256()
+        target = util.uint256_from_compact(self.nBits)
+        if self.sha256 > self.target:
+            return False
+        hashes = [b'\0' * 0x20]
+        for tx in self.vtx[1:]:
+            hashes.append(
+                SHA256.new(SHA256.new(tx.serialize()).digest()).digest())
+        while len(hashes) > 1:
+            newhashes = []
+            for i in xrange(0, len(hashes), 2):
+                i2 = min(i + 1, len(hashes) - 1)
+                newhashes.append(SHA256.new(
+                    SHA256.new(hashes[i] + hashes[i2]).digest()).digest())
+            hashes = newhashes
+        calcwitness = SHA256.new(
+            SHA256.new(hashes[0] + witness_nonce).digest()).digest()
+        if calcwitness != self.witness:
+            return False
+        return True
